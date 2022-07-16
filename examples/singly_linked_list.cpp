8e08d3e2109d067506bab240033343e3c56575f9
Arghadeep <arghadeep.mazumder@teraki.com>
2022-07-16 21:55:49 +0200

    minor addition

--- examples/singly_linked_list.cpp
+++ examples/singly_linked_list.cpp
@@ -1,27 +1,51 @@
+//============================================================================
+// Name        : singly_linked_list.cpp
+// Author      : Arghadeep Mazumder
+// Version     : 1.0
+// Copyright   : -
+// Description :
+//============================================================================
+#pragma once
 #include <iostream>
 #include <gtest/gtest.h>
 #include "singly_linked_list.hpp"
 
 int main(int argc, char **argv) {
   using Node = Node<int>;
+  // Creating a linked list with 3 nodes
   // Creating the last node first
   auto *third = new Node(3);
   auto *second = new Node(1, third);
+  // Creating the head at the last
   auto *head = new Node(1, second);
-
+  // Printing the linked list
   Node::printList(head);
+  // Counting the number of nodes in the linked list
   Node::getNodesCount(head);
+  // Inserting an element at the front of the linked list
   Node::insertAtFront(&head, 1);
+  // Printing the linked list
   Node::printList(head);
+  // Inserting an element at the end of the linked list
   Node::insertAtLast(&head,9);
+  // Printing the linked list
   Node::printList(head);
+  // Inserting an element at a desired position in the linked list
   Node::insertAnywhere(head->next, 9);
+  // Printing the linked list
   Node::printList(head);
+  // Counting the number of nodes in the linked list
   Node::getNodesCount(head);
+  // Finding duplicate elements in the linked list
   Node::findDuplicate(head);
+  // Detecting a loop in the linked list
   Node::detectLoop(head);
+  // Deleting the first node of the linked list
   Node::deleteFirstNode(&head);
+  // Printing the linked list
   Node::printList(head);
+  // Deleting the last node of the linked list
   Node::deleteLastNode(&head);
+  // Printing the linked list
   Node::printList(head);
 }
