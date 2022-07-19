0943e2826c05d88b3c6e4b7c75829a229ee82109
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2022-07-19 11:00:25 +0200

    utils added

---
+++ include/utils/colors.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#define RESET "\033[0m"
+#define RED "\033[31m"    /* Red */
+#define GREEN "\033[32m"  /* Green */
+#define YELLOW "\033[33m" /* Yellow */
+#define BLUE "\033[34m"   /* Blue */
+#define MAGENTA "\033[35m"   /* Blue */
+#define CYAN "\033[36m"   /* Blue */
+#define BOLD "\033[1m"
+#define WHITE "\033[37m"
+#define UNDERLINE "\033[4m"
