6861b46717d30a2649d375547d9660486aaea6f4
Arghadeep <arghadeep.mazumder@teraki.com>
2022-07-20 11:57:09 +0200

    first node deletion added

--- examples/doubly_linked_list.cpp
+++ examples/doubly_linked_list.cpp
@@ -10,10 +10,11 @@
 
 int main(int argc, char **argv) {
   using Node = dataStructure::doublyLinkedList::Node<int>;
-  //Head
-  Node* head = new Node(1);
-  Node* second = new Node(2);
-  Node* third = new Node(3);
+  // Creating a doubly linked list:  1 <-> 2 <-> 3
+  // Head
+  Node *head = new Node(1);
+  Node *second = new Node(2);
+  Node *third = new Node(3);
 
   third->next = nullptr;
   second->next = third;
@@ -22,11 +23,25 @@ int main(int argc, char **argv) {
   second->prev = head;
   third->prev = second;
 
+  std::cout << BOLD << GREEN << " ------- INSERTION -------" << std::endl;
+  // Printing the elements in the linked list
   Node::printAllElements(head);
+  // Inserting a node at front: 4 <-> 1 <-> 2 <-> 3
   Node::insertAtFront(&head, 4);
+  // Printing the elements in the linked list
   Node::printAllElements(head);
+  // Inserting a node at back: 4 <-> 1 <-> 2 <-> 3 <-> 7
   Node::insertAtBack(&head, 7);
+  // Printing the elements in the linked list
   Node::printAllElements(head);
+  // Inserting a node anywhere: 4 <-> 1 <-> 2 <-> 3 <-> 7
   Node::insertAnywhere(third->next, 8);
+  // Printing the elements in the linked list
+  Node::printAllElements(head);
+
+  std::cout << BOLD << RED << " ------- DELETION -------" << std::endl;
+  // Deleting First Node
+  Node::deleteFirstNode(&head);
+  // Printing the elements in the linked list
   Node::printAllElements(head);
 }
