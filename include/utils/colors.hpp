101f21a17247bf38d759541a86680580f80ea29b
Arghadeep <arghadeep.mazumder@teraki.com>
2022-07-20 16:34:15 +0200

    messages added

--- include/utils/colors.hpp
+++ include/utils/colors.hpp
@@ -1,12 +1,19 @@
+//============================================================================
+// Name        : colors.hpp
+// Author      : Arghadeep Mazumder
+// Version     : 1.0
+// Copyright   : -
+// Description :
+//============================================================================
 #pragma once
 
 #define RESET "\033[0m"
-#define RED "\033[31m"    /* Red */
-#define GREEN "\033[32m"  /* Green */
-#define YELLOW "\033[33m" /* Yellow */
-#define BLUE "\033[34m"   /* Blue */
-#define MAGENTA "\033[35m"   /* Blue */
-#define CYAN "\033[36m"   /* Blue */
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
 #define BOLD "\033[1m"
 #define WHITE "\033[37m"
 #define UNDERLINE "\033[4m"
---
+++ include/utils/message.hpp
@@ -0,0 +1,51 @@
+//============================================================================
+// Name        : message.hpp
+// Author      : Arghadeep Mazumder
+// Version     : 1.0
+// Copyright   : -
+// Description :
+//============================================================================
+#pragma once
+
+#include "colors.hpp"
+#include <iostream>
+
+// -------- WARNING --------
+template <typename T> void WARNING(T t) {
+  std::cout << BOLD << RED << t << std::endl;
+}
+
+template <typename T, typename... Args> void WARNING(T t, Args... args) {
+  std::cout << BOLD << RED << t << std::endl;
+  WARNING(args...);
+}
+
+// -------- STATUS --------
+template <typename T, typename C> void STATUS(T t, C c) {
+  std::cout << c << t << std::endl;
+}
+
+template <typename T, typename C, typename... Args>
+void STATUS(T t, C c, Args... args) {
+  std::cout << c << t << std::endl;
+  STATUS(c, args...);
+}
+
+// -------- MESSAGE --------
+template <typename T> void MESSAGE(T t) { std::cout << CYAN << t << std::endl; }
+
+template <typename T, typename... Args> void MESSAGE(T t, Args... args) {
+  std::cout << CYAN << t << std::endl;
+  MESSAGE(args...);
+}
+
+// -------- PRINT --------
+template <typename T> void PRINT(T t) {
+  std::cout << MAGENTA << t << " ";
+}
+
+template <typename T, typename... Args> void PRINT(T t, Args... args) {
+  std::cout << MAGENTA << t << " ";
+  PRINT(args...);
+  std::cout << std::endl;
+}
