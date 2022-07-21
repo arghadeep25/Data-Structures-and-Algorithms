6c636b1b0148a8c813df99a1a85940e9359fe904
Arghadeep <arghadeep.mazumder@teraki.com>
2022-07-21 18:42:20 +0200

    sorting algo initial commit

---
+++ include/sort/selection_sort.hpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : selection_sort.hpp
+// Author      : Arghadeep Mazumder
+// Version     : 1.0
+// Copyright   : -
+// Description :
+//============================================================================
+#pragma once
+
+#include<iostream>
+#include "utils/message.hpp"
+
+template<typename Data>
+class SelectionSort{
+
+};
