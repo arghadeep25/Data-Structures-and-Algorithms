08326603fe943afcb0d9c111cd710bd1f3eba543
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2022-07-22 15:12:41 +0200

    set and get vector added

--- include/sort/base.hpp
+++ include/sort/base.hpp
@@ -12,10 +12,32 @@
 
 namespace dataStructure::sort {
 template <typename DataType> class Sort {
+  // Default Contructor
 public:
-  virtual void printVector(const std::vector<DataType> &) = 0;
+  explicit Sort() = default;
 
 public:
-  virtual std::vector<DataType> sort(const std::vector<DataType> &) = 0;
+  // Function to print the vector
+  virtual void printVector() {
+    std::cout << "Elements in Vector:: ";
+    for (const auto &elem : data)
+      std::cout << elem << " ";
+    std::cout << std::endl;
+  }
+
+public:
+  // Function to set the data for sorting
+  void setVector(const std::vector<DataType> &vec) { data = vec; }
+
+public:
+  // Function to return the original data
+  std::vector<DataType> getVector() { return data; }
+
+public:
+  // Virtual function for sorting
+  virtual std::vector<DataType> sort() = 0;
+
+protected:
+  std::vector<DataType> data;
 };
 } // namespace dataStructure::sort
