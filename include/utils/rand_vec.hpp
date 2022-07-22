80de4373e05e7404dd0e1e1f29ec91e940e71368
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2022-07-22 14:38:28 +0200

    namespace added

--- include/utils/rand_vec.hpp
+++ include/utils/rand_vec.hpp
@@ -13,21 +13,24 @@
 #include <random>
 #include <vector>
 
+namespace dataStructure::Utils {
 // Function to generate a random vector
 // Params:-
 // size: size of the vector
 // minVal = minimum value from which distribution will start
 // maxVal = minimum value from which distribution will end
-template <typename Data>
-std::vector<Data> generateRandomVector(const int size, const Data minVal = 0,
-                                       const Data maxVal = 100) {
+template <typename DataType>
+std::vector<DataType> generateRandomVector(const int size,
+                                           const DataType minVal = 0,
+                                           const DataType maxVal = 100) {
   std::random_device rndDevice;
   std::mt19937 mersenneEngine{rndDevice()};
-  std::uniform_int_distribution<Data> distribution{minVal, maxVal};
+  std::uniform_int_distribution<DataType> distribution{minVal, maxVal};
   auto generate = [&distribution, &mersenneEngine]() {
     return distribution(mersenneEngine);
   };
-  std::vector<Data> randVec(size);
+  std::vector<DataType> randVec(size);
   std::generate(std::begin(randVec), std::end(randVec), generate);
   return randVec;
 }
+} // namespace dataStructure::Utils
