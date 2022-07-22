ffd3f94e866e9e368c6e02f612faa95b5ff5666f
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2022-07-22 14:37:42 +0200

    selection sort class added

--- include/sort/selection_sort.hpp
+++ include/sort/selection_sort.hpp
@@ -7,10 +7,23 @@
 //============================================================================
 #pragma once
 
-#include<iostream>
+#include "base.hpp"
 #include "utils/message.hpp"
+#include <iostream>
 
-template<typename Data>
-class SelectionSort{
+namespace dataStructure::sort {
+template <typename DataType> class SelectionSort : public Sort<DataType> {
+public:
+  explicit SelectionSort() = default;
 
+public:
+  void printVector(const std::vector<DataType> &data) override {
+    for (const auto &elem : data)
+      std::cout << elem << " ";
+    std::cout << std::endl;
+  }
+
+public:
+  std::vector<DataType> sort(const std::vector<DataType> &data) override {}
 };
+} // namespace dataStructure::sort
