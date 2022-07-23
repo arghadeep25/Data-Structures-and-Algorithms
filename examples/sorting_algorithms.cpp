743621e2620f671432eab2811506ff62c615f6d9
Arghadeep <arghadeep.mazumder@teraki.com>
2022-07-23 20:44:29 +0200

    bubble sort example added

--- examples/sorting_algorithms.cpp
+++ examples/sorting_algorithms.cpp
@@ -9,15 +9,23 @@
 #include "utils/rand_vec.hpp"
 
 int main(int argc, char **argv) {
+  // Generating a random vector
   std::vector<int> data =
       dataStructure::Utils::generateRandomVector(15, 0, 100);
-  const char *sort_type = "Selection_Sort";
+  // Name of the sorting algorithm
+//  const char *sort_type = "Selection_Sort";
+  const char *sort_type = "Bubble_Sort";
   MESSAGE("Selected Sort Type: ", sort_type);
+  // Passing the sorting method to the sorting algorithms factory
   auto sort_method = dataStructure::sort::sortMethod<int>(sort_type);
+  // Passing the vector
   sort_method->setVector(data);
   MESSAGE("------- BEFORE SORTING -------");
+  // Printing the vector before sorting
   sort_method->printVector();
+  // Sort the vector
   sort_method->sort();
   MESSAGE("------- AFTER SORTING -------");
+  // Printing the vector after sorting
   sort_method->printVector();
 }
